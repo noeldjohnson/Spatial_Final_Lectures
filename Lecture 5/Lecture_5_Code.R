bbc3a0e9d37ab031f9486c86397716b5bbe32475
noeldjohnson <39693544+noeldjohnson@users.noreply.github.com>
2021-09-29 20:06:57 -0400

    updates

--- Lecture 5/Lecture_5_Code.R
+++ Lecture 5/Lecture_5_Code.R
@@ -8,14 +8,21 @@ library(sf)
 library(stargazer)
 
 # Three methods...
-# (1) Geocoding: Turning a name of some geographical feature into coordinates, a line or a polygon. ==> This can be done in R
+
+# (1) Geocoding: Turning a name of some geographical feature into coordinates (or the reverse). ==> This can be done in R
+
 # (2) Georeferencing: Taking an image of some place and giving it a known reference system. ==> QGIS or ARC
+
 # (3) Digitizing: Taking a georeferenced image and extracting the vector features in some way. ==> QGIS or ARC
 
+##################################################
 
 # (1) Geocoding: See slides...
+
 # An example using tmap’s geocode_OSM
+
 library(tmaptools)
+
 moshi <- geocode_OSM("Moshi, Tanzania", as.sf = T,
             details = F) %>% glimpse()
 
